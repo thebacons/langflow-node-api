f2b192d51edf2f0a2751adb6de81f0021b7f2f58
Colin Bacon <77930793+thebacons@users.noreply.github.com>
2024-09-24 18:47:12 +0200

    Update index.js
    Port chamged to 3001

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post('/run-flow', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
